c5a3c126fe05d6b1333ea0f6744b25a48f1d68ed
aryansinguzab-lab <aryansinguzab@gmail.com>
2026-01-23 23:08:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,36 @@
-const cacheName = 'lifemgr-v10'; // Jump to v10 to force a change
+const cacheName = 'lifemgr-v1';
 const assets = [
   './',
   './index.html',
   './manifest.json',
-  './manifest.js',
   './dexie.js',
   './chart.js',
   './icon.png'
 ];
 
 self.addEventListener('install', evt => {
-  self.skipWaiting(); // Kill the old version immediately
   evt.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
       );
     })
   );
-  return self.clients.claim(); // Take control of the page NOW
 });
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      // Return cache, but also update the cache in the background (Stale-While-Revalidate)
-      const fetchResult = fetch(evt.request).then(fetchRes => {
-        return caches.open(cacheName).then(cache => {
-          if (evt.request.method === 'GET') {
-            cache.put(evt.request.url, fetchRes.clone());
-          }
-          return fetchRes;
-        });
-      });
-      return cacheRes || fetchResult;
+    caches.match(evt.request).then(res => {
+      return res || fetch(evt.request);
     })
   );
 });
