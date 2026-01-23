74f1bb8e623124405402604d5f43b210fb82dfb8
aryansinguzab-lab <aryansinguzab@gmail.com>
2026-01-23 22:22:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'lifemgr-v5'; // Increased version
+const cacheName = 'lifemgr-v10'; // Jump to v10 to force a change
 const assets = [
   './',
   './index.html',
@@ -9,49 +9,37 @@ const assets = [
   './icon.png'
 ];
 
-// Install: Cache everything immediately
 self.addEventListener('install', evt => {
-  self.skipWaiting();
+  self.skipWaiting(); // Kill the old version immediately
   evt.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Caching shell assets');
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-// Activate: Delete old versions of the app
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
       );
     })
   );
-  return self.clients.claim();
+  return self.clients.claim(); // Take control of the page NOW
 });
 
-// Fetch: Serve from Cache first for speed
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      // Return cached file if found, else fetch from network
-      return cacheRes || fetch(evt.request).then(fetchRes => {
+      // Return cache, but also update the cache in the background (Stale-While-Revalidate)
+      const fetchResult = fetch(evt.request).then(fetchRes => {
         return caches.open(cacheName).then(cache => {
-          // Dynamically cache new requests (like images/fonts)
           if (evt.request.method === 'GET') {
             cache.put(evt.request.url, fetchRes.clone());
           }
           return fetchRes;
         });
       });
-    }).catch(() => {
-      // Fallback if network and cache fail (Offline)
-      if (evt.request.url.indexOf('.html') > -1) {
-        return caches.match('./index.html');
-      }
+      return cacheRes || fetchResult;
     })
   );
 });
